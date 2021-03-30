279bae6ba2bc1f93dc12c833fea776a8ed6efad1
dichaputra <arwindra23@gmail.com>
2021-03-30 12:55:03 +0700

    fungsi kunjungan
    penambahan fungsi kunjungan

--- app/Http/Controllers/KunjunganController.php
+++ app/Http/Controllers/KunjunganController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\kunjungan;
 use App\Models\customer;
 use App\Models\target;
+use Illuminate\Support\Facades\DB;
 
 class KunjunganController extends Controller {
 
@@ -25,8 +26,33 @@ class KunjunganController extends Controller {
 
     public function index()
     {
+        //--------------------
         //Page depan Kunjungan
-        return view('kunjungan');
+        //---------------------
+
+        $tar = target::first();
+        $target = $tar->target;
+        $tanggal = date('d');
+        $bulan = date('m');
+        $tahun = date('Y');
+
+        $jmlKunjungan = kunjungan::where('tanggal', '=', $tanggal)
+                        ->where('bulan', '=', $bulan)
+                        ->where('tahun', '=', $tahun)
+                        ->where('user_id', '=', session('userid'))
+                        ->groupBy('user_id')->count();
+
+        $sisakunjungan = $target - $jmlKunjungan;
+
+        //echo "target = $target | jumlah kunjungan saat ini = $jmlKunjungan";
+        // ==============================================================
+        $alldailykunjungan = kunjungan::where('tanggal', '=', $tanggal)
+                        ->where('bulan', '=', $bulan)
+                        ->where('tahun', '=', $tahun)
+                        ->where('user_id', '=', session('userid'))->get();
+
+        return view('kunjungan', ["alldailykunjungan" => $alldailykunjungan, 'sisakunjungan' => $sisakunjungan, 'target' => $target,
+            'todaykunjungan' => $jmlKunjungan]);
     }
 
     public function TambahKunjunganForm()
@@ -38,17 +64,16 @@ class KunjunganController extends Controller {
     public function insertKunjungan(Request $request)
     {
         $kunjungan = $request->kunjungan;
+        $tar = target::first();
+        
         if ($kunjungan == 'sudah')
         {
-
-            $tar = target::first();
-            
 //            echo "$namapic | $kontakpic | $produk | $kisaranharga | $waktu | $lainlain | $filefoto";
             $dbkunjungan = new kunjungan;
             $dbkunjungan->user_id = session('userid');
             $dbkunjungan->nama_perusahaan = $request->customer;
             $dbkunjungan->target = $tar->target;
-            $dbkunjungan->tanggal= date('d');
+            $dbkunjungan->tanggal = date('d');
             $dbkunjungan->bulan = date('m');
             $dbkunjungan->tahun = date('Y');
             $dbkunjungan->nama_pic = $request->namapic;
@@ -58,14 +83,25 @@ class KunjunganController extends Controller {
             $dbkunjungan->waktu_pembelian = $request->waktu;
             $dbkunjungan->lainlain = $request->lainlain;
             $dbkunjungan->foto = $request->filefoto->store('public/fotokunjungan');
+            $dbkunjungan->status = 'tercapai'; // tercapai|tidak tercapai|ditolerir|tidak ditolerir
             $dbkunjungan->save();
 
             return redirect()->route('kunjungan');
         }
         elseif ($kunjungan = 'belum')
         {
-            $alasan = $request->alasan;
-            dd($alasan);
+
+            $dbkunjungan = new kunjungan;
+            $dbkunjungan->user_id = session('userid');
+            $dbkunjungan->nama_perusahaan = 'Tidak Melakukan Kunjungan';
+            $dbkunjungan->target = $tar->target;
+            $dbkunjungan->tanggal = date('d');
+            $dbkunjungan->bulan = date('m');
+            $dbkunjungan->tahun = date('Y');
+            $dbkunjungan->status = 'tidak tercapai'; // tercapai|tidak tercapai|ditolerir|tidak ditolerir
+            $dbkunjungan->save();
+//            $alasan = $request->alasan;
+            return redirect()->route('kunjungan');
         }
     }
 
